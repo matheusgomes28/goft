a3e30a6dc99965a8f521b7328f393aa3e1cfbd9b
= <=>
2021-03-24 21:01:24 +0000

    vector multiply func plus tests

--- goft.go
+++ goft.go
@@ -1,5 +1,24 @@
 package goft
 
-func Hello() string {
-	return "Hello World"
+import "fmt"
+
+type vector []float32
+type matrix [][]float32
+
+// multiply two vectors of the same size together
+func (v1 vector) multiply(v2 vector) (vector, error) {
+
+	v1Len := len(v1)
+	v2Len := len(v2)
+
+	if v1Len != v2Len {
+		return nil, fmt.Errorf("vectors aren't the same size: v1 size = %d, v2 size = %d", v1Len, v2Len)
+	}
+
+	v3 := make(vector, v1Len)
+	for i := 0; i < v1Len; i++ {
+		v3[i] = v1[i] * v2[i]
+	}
+
+	return v3, nil
 }
--- goft_test.go
+++ goft_test.go
@@ -1,12 +1,43 @@
 package goft
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestHello(t *testing.T) {
-	actual := Hello()
-	expected := "Hello World"
+func TestMultiply(t *testing.T) {
 
-	if actual != expected {
-		t.Errorf("Unexpected response:\nGot: %s, Wanted: %s", actual, expected)
+	cases := []struct {
+		name     string
+		v1       vector
+		v2       vector
+		expected vector
+	}{
+		{
+			"Succes Ints 1",
+			vector{1, 2, 3, 4},
+			vector{4, 3, 2, 1},
+			vector{4, 6, 6, 4},
+		},
+		{
+			"Success Floats 1.2",
+			vector{1, 2, 3.3, 4.7},
+			vector{4.2, 3.3, 2, 1},
+			vector{4.2, 6.6, 6.6, 4.7},
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+
+			actual, err := tt.v1.multiply(tt.v2)
+			if err != nil {
+				t.Errorf("Unexpected error: %s", err)
+			}
+
+			if !reflect.DeepEqual(actual, tt.expected) {
+				t.Errorf("Unexpected response:\nGot: %v, Wanted: %v", actual, tt.expected)
+			}
+		})
 	}
 }
